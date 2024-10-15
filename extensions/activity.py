98ab2d5391a85496c0ba83205d52b0ed8bc741e7
minz38 <minz-the-furry@mailbox.org>
2024-10-15 15:27:58 +0200

    Voicetimes fixed, embed extended

--- extensions/activity.py
+++ extensions/activity.py
@@ -13,69 +13,28 @@ logger = LoggerManager(name="Inactivity", level="INFO", log_file="logs/Inactivit
 # Inactivity Bot extension inside this class
 class Inactivity(commands.Cog):
     def __init__(self, bot):
-        """
-        Initialize the Inactivity cog with the provided bot instance.
-
-        Parameters:
-        bot (discord.ext.commands.Bot): The bot instance to which this cog will be added.
-
-        Returns:
-        None
-        """
         self.bot = bot
         self.voice_channel_join_times = {}
+        self.active_guilds = []
 
         # Ensure the activity folder exists
         if not os.path.exists('activity'):
             os.makedirs('activity')
-            logger.debug("Created activity folder")
+            logger.info("Created activity folder")
 
-        # Load data for each guild | also creates null entries for each user if they don't exist
-        for guild in bot.guilds:
-            self.initialize_guild_activity(guild.id)
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        """
-        Listener for message events. Updates user activity in the activity log when a message is sent.
-
-        Parameters:
-        message (discord.Message): The message object that triggered the event.
-
-        Returns:
-        None
-        """
-        
-        # Only track if the message is not from a bot and is inside a server
-        if not message.author.bot and message.guild:
-            self.update_user_activity(
-                message.guild.id,
-                message.author.id,
-                {"message_count": 1}
-            )
+    async def cog_load(self):
+        await self.load_active_guilds()
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        """
-        Listener for voice state update events. Tracks user's voice channel activity.
-
-        Parameters:
-        member (discord.Member): The member object that triggered the event.
-        before (discord.VoiceState): The voice state before the update.
-        after (discord.VoiceState): The voice state after the update.
-
-        Returns:
-        None
-        """
-        
         if member.bot:
             return
 
         # User joins a voice channel
         if before.channel is None and after.channel is not None:
             # Get the guild from the after state (since they just joined)
-            guild_id = after.channel.guild.id
-            self.voice_channel_join_times[member.id] = datetime.datetime.now()
+            time = datetime.datetime.now()
+            self.voice_channel_join_times[member.id] = time
 
         # User leaves the voice channel
         elif before.channel is not None and after.channel is None:
@@ -85,120 +44,100 @@ class Inactivity(commands.Cog):
 
             if join_time:
                 duration = int((datetime.datetime.now() - join_time).total_seconds())
-                self.update_user_activity(
-                    guild_id,
-                    member.id,
-                    {"voice_channel_time": duration}
-                )
+                await self.store_voice_times(guild_id=guild_id, member=member, time=duration)
 
-    @staticmethod
-    def load_activity_data(guild_id):
-        """Load the activity data from the JSON file."""
-        file_path = f'activity/{guild_id}.json'
-        if not os.path.exists(file_path):
-            return {}
-        with open(file_path, 'r') as f:
-            return json.load(f)
+    async def store_voice_times(self, guild_id, member, time):
+        if guild_id in self.active_guilds:
+            # File path where the activity data is stored
+            file_path = f'activity/{guild_id}.json'
 
-    def initialize_guild_activity(self, guild_id):
-        """Initialize the activity log for a guild."""
-        file_path = f'activity/{guild_id}.json'
-
-        # If the file doesn't exist, create an empty structure
-        if not os.path.exists(file_path):
-            with open(file_path, 'w') as f:
-                json.dump({}, f)  # Empty dict to start
-
-        # Load existing data
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-
-        # Add missing users to the JSON file
-        for guild in self.bot.guilds:
-            if guild.id == guild_id:
-                for member in guild.members:
-                    if not member.bot and str(member.id) not in data:
-                        data[str(member.id)] = {
-                            "year": {
-                                str(datetime.datetime.now().year): {
-                                    "month": {
-                                        str(datetime.datetime.now().month): {
-                                            "message_count": 0,
-                                            "voice_channel_time": 0,
-                                            "last_activity": None
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-        # Save the updated data back to the file
-        with open(file_path, 'w') as f:
-            json.dump(data, f, indent=4)
-        logger.debug(f"Activity log for guild {guild_id} initialized")
-
-    @staticmethod
-    def update_user_activity(guild_id, user_id, update_data):
-        """
-        Update the activity log for a user.
-
-        Parameters:
-        guild_id (int): The ID of the guild for which the activity log is being updated.
-        user_id (int): The ID of the user whose activity log is being updated.
-        update_data (dict): A dictionary containing the keys and values to be updated in the user's activity log.
-
-        Returns:
-        None
-
-        The function reads the activity log for the specified guild and user, updates the relevant data, and saves the
-        updated log back to the file.
-        If the user's data is not present in the log, it initializes the user's data with default values.
-        """
-        file_path = f'activity/{guild_id}.json'
-
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-
-        current_year = str(datetime.datetime.now().year)
-        current_month = str(datetime.datetime.now().month)
-
-        # Initialize user data if not present
-        if str(user_id) not in data:
-            data[str(user_id)] = {
-                "year": {
-                    current_year: {
-                        "month": {
-                            current_month: {
-                                "message_count": 0,
-                                "voice_channel_time": 0,
-                                "last_activity": None
-                            }
-                        }
-                    }
-                }
-            }
-
-        # Update activity data for the current year and month
-        year_data = data[str(user_id)]["year"].setdefault(current_year, {"month": {}})
-        month_data = year_data["month"].setdefault(current_month, {
-            "message_count": 0,
-            "voice_channel_time": 0,
-            "last_activity": None
-        })
-
-        # Apply updates
-        for key, value in update_data.items():
-            if key in month_data:
-                month_data[key] += value  # Increment counts
+            # Load existing data from the file if it exists, or initialize an empty dictionary
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as f:
+                    voice_data = json.load(f)
             else:
-                month_data[key] = value
+                voice_data = {}
 
-        # Update last activity timestamp
-        month_data["last_activity"] = datetime.datetime.now().strftime('%d.%m.%Y')
+            user_id = str(member.id)  # Use the user ID as a string
+            date = datetime.datetime.now().strftime("%Y-%m")  # Get the current year and month in YYYY-MM format
 
-        # Save the updated data back to the file
-        with open(file_path, 'w') as f:
-            json.dump(data, f, indent=4)
+            # If the user doesn't have an entry in the JSON, initialize it
+            if user_id not in voice_data:
+                voice_data[user_id] = {}
+
+            # If the user doesn't have an entry for the current month, initialize it
+            if date not in voice_data[user_id]:
+                voice_data[user_id][date] = {"voice_times": 0}
+
+            # Add the duration to the existing time
+            voice_data[user_id][date]["voice_times"] += time
+
+            # Save the updated data back to the file
+            with open(file_path, 'w') as f:
+                json.dump(voice_data, f, indent=4)
+
+    async def load_active_guilds(self):
+        """Load active guilds with voice tracking enabled."""
+        logger.info("Loading active guilds with voice tracking enabled.")
+        for file in os.listdir('configs/guilds'):
+            if file.endswith('.json'):
+                with open(f'configs/guilds/{file}', 'r') as f:
+                    guild_config = json.load(f)
+                    # If 'voice_tracking' is in active_extensions, add the guild_id to active_guilds
+                    if "voice_tracking" in guild_config.get('active_extensions', []):
+                        self.active_guilds.append(int(file.split('.')[0]))
+        print(f"Active guilds: {self.active_guilds}")  # Debugging purpose
+        logger.info(f"Total active guilds loaded: {len(self.active_guilds)}")
+
+    @app_commands.command(name="vc_tracking", description="Setup the Voicechannel Tracking feature.")
+    @app_commands.allowed_contexts(guilds=True, dms=False, private_channels=False)
+    @app_commands.allowed_installs(guilds=True, users=False)
+    @app_commands.default_permissions(manage_guild=True)
+    @app_commands.choices(operation=[app_commands.Choice(name="Enable", value=1),
+                                     app_commands.Choice(name="Disable", value=0),
+                                     app_commands.Choice(name="Status", value=2)])
+    async def voice_tracking(self, interaction: discord.Interaction, operation: int):
+        match operation:
+            case 1:  # Enable
+                guild_id = interaction.guild.id
+                if guild_id not in self.active_guilds:
+                    self.active_guilds.append(guild_id)
+                    # update active_extension in the guild config file configs/guilds/{guild_id}.json
+                    with open(f'configs/guilds/{guild_id}.json', 'r') as f:
+                        guild_config = json.load(f)
+                        guild_config['active_extensions'].append('voice_tracking')
+                        with open(f'configs/guilds/{guild_id}.json', 'w') as f:
+                            json.dump(guild_config, f, indent=4)
+                            logger.info(f"Voice tracking enabled for guild: {interaction.guild.name} ({guild_id})")
+                    await interaction.response.send_message(content="Voice tracking enabled successfully.")  # noqa
+                    print(self.active_guilds)  # Debugging purpose
+                else:
+                    await interaction.response.send_message(content="Voice tracking is already enabled for this guild.")  # noqa
+                    logger.warning(f"Voice tracking already enabled for guild: {interaction.guild.name} ({guild_id})")
+
+            case 0:  # Disable
+                guild_id = interaction.guild.id
+                if guild_id in self.active_guilds:
+                    self.active_guilds.remove(guild_id)
+                    with open(f'configs/guilds/{guild_id}.json', 'r') as f:
+                        guild_config = json.load(f)
+                        guild_config['active_extensions'].remove('voice_tracking')
+                        with open(f'configs/guilds/{guild_id}.json', 'w') as f:
+                            json.dump(guild_config, f, indent=4)
+                            logger.info(f"Voice tracking disabled for guild: {interaction.guild.name} ({guild_id})")
+                            await interaction.response.send_message(content="Voice tracking disabled successfully.") # noqa
+                else:
+                    await interaction.response.send_message(content= # noqa
+                                                            "Voice tracking is already disabled for this guild.")
+                    logger.warning(f"Voice tracking already disabled for guild: {interaction.guild.name} ({guild_id})")
+            case 2:  # status
+                if int(interaction.guild_id) in self.active_guilds:
+                    await interaction.response.send_message(content="Voice tracking is enabled for this guild.")  # noqa
+                else:
+                    await interaction.response.send_message(content="Voice tracking is disabled for this guild.")  # noqa
+
+            case __:
+                raise ValueError("Invalid operation. Please choose 'Enable' or 'Disable'.")
 
     @app_commands.command(name="inactivity_check", description="list inactive users.")
     @app_commands.allowed_contexts(guilds=True, dms=False, private_channels=False)
@@ -210,11 +149,20 @@ class Inactivity(commands.Cog):
     async def inactivity_check(self, interaction: discord.Interaction, days: int = 30):
         channel_counter = 0
         message_counter = 0
-        past_month = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=days)
+        past_date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=days)
         last_message_list = {}
+        voice_times = {}
 
         # Defer the interaction to prevent timeout
         await interaction.response.defer(thinking=True)  # noqa
+
+        # Load voice activity data from the JSON file for the guild
+        guild_id = str(interaction.guild.id)
+        voice_data_file = f'activity/{guild_id}.json'
+
+        if os.path.isfile(voice_data_file):
+            with open(voice_data_file, 'r') as f:
+                voice_times = json.load(f)
 
         try:
             for channel in interaction.guild.text_channels:  # Iterate only over text channels
@@ -225,15 +173,12 @@ class Inactivity(commands.Cog):
                     continue
 
                 # Fetch channel history after the specified date
-                async for message in channel.history(after=past_month):
+                async for message in channel.history(after=past_date):
 
                     # Skip messages from bots
                     if message.author.bot:
                         continue
                     message_counter += 1
-                    # Log each message processed for debugging purposes
-                    logger.debug(f"Message from {message.author} at {message.created_at} in {channel.name}")
-
                     # Track the most recent message timestamp for each user
                     if message.author.id not in last_message_list:
                         last_message_list[message.author.id] = message.created_at
@@ -246,29 +191,49 @@ class Inactivity(commands.Cog):
             await interaction.response.send_message(content="Failed to retrieve channel history.")  # noqa
             return
 
-        # Find users who haven't sent a message in the last {days} days
+        # Find users who haven't sent a message in the last {days} days and gather their voice times
         inactive_users = []
         for member in interaction.guild.members:
             if member.bot:
                 continue
             last_message_time = last_message_list.get(member.id)
-            if not last_message_time or last_message_time < past_month:
-                inactive_users.append(member)
+            if not last_message_time or last_message_time < past_date:
+                # Get the total voice time for this user over the past {days}
+                total_voice_seconds = 0
+                user_id = str(member.id)
 
+                if user_id in voice_times:
+                    # Loop through the months that are relevant for the specified days
+                    for year_month, voice_data in voice_times[user_id].items():
+                        year, month = map(int, year_month.split("-"))
+
+                        # Make activity_date timezone-aware by attaching the UTC timezone
+                        activity_date = datetime.datetime(year, month, 1, tzinfo=datetime.timezone.utc)
+
+                        # Check if the activity date falls within the last {days}
+                        if activity_date >= past_date.replace(day=1):  # Compare with the first day of the month
+                            total_voice_seconds += voice_data.get("voice_times", 0)
+
+                # Convert total voice time to hours and minutes
+                hours, remainder = divmod(total_voice_seconds, 3600)
+                minutes = remainder // 60
+
+                inactive_users.append((member, hours, minutes))
+
+        # Create the embed
         if inactive_users:
-            # user_list = "\n".join([member.display_name for member in inactive_users])
-            mention_member_list = "\n".join([f"<@{member.id}>" for member in inactive_users])
+            mention_member_list = "\n".join([
+                f"<@{member.id}> - Voice Time: {hours}h {minutes}m"
+                for member, hours, minutes in inactive_users
+            ])
             embed = discord.Embed(title=f"List of Inactive Users in the last {days} days",
-                                  description=
-                                  f"""
-                                  Processed **{message_counter}** in **{channel_counter}** channels and found
-                                  **{len(inactive_users)}** users who haven't sent a message in the last **{days}** 
-                                  days. 
-                                  """)
+                                  description=f"Processed **{message_counter}** messages in **{channel_counter}** "
+                                              f"channels and found **{len(inactive_users)}** users who haven't sent "
+                                              f"a message in the last **{days}** days.",
+                                  color=discord.Color.blue())
             embed.add_field(name="Inactive Users", value=mention_member_list, inline=False)
-            embed.set_footer(text=f"created by {interaction.user.name}")
+            embed.set_footer(text=f"Created by {interaction.user.name}")
             await interaction.followup.send(embed=embed)
-
         else:
             await interaction.followup.send(content="No inactive users found in the last 30 days.")  # noqa
 
