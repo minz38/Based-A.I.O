88e1acd90af18bfbb1195a0c4fb19885749b60ef
minz3 <minz3@mailbox.org>
2025-04-14 08:18:08 +0000

    logging -> l.

--- dep/logger.py
+++ dep/logger.py
@@ -1,4 +1,4 @@
-import logging
+import logging as l
 from os import getenv
 from pathlib import Path
 
@@ -32,27 +32,27 @@ class LoggerManager:
         logger_name = name or log_name
         self.logger = self._get_logger(logger_name, level, self.log_file)
 
-    def _get_logger(self, name: str, level: str, log_file: Path) -> logging.Logger:
+    def _get_logger(self, name: str, level: str, log_file: Path) -> l.Logger:
         if name in self._loggers:
             return self._loggers[name]
 
         level_dict = {
-            "DEBUG": logging.DEBUG,
-            "INFO": logging.INFO,
-            "WARNING": logging.WARNING,
-            "ERROR": logging.ERROR,
-            "CRITICAL": logging.CRITICAL
+            "DEBUG": l.DEBUG,
+            "INFO": l.INFO,
+            "WARNING": l.WARNING,
+            "ERROR": l.ERROR,
+            "CRITICAL": l.CRITICAL
         }
-        log_level = level_dict.get(level.upper(), logging.INFO)
+        log_level = level_dict.get(level.upper(), l.INFO)
 
-        logger = logging.getLogger(name)
+        logger = l.getLogger(name)
         logger.setLevel(log_level)
 
         if not logger.hasHandlers():
             # File Handler
-            file_handler = logging.FileHandler(log_file, encoding='utf-8')
+            file_handler = l.FileHandler(log_file, encoding='utf-8')
             file_handler.setLevel(log_level)
-            file_formatter = logging.Formatter(
+            file_formatter = l.Formatter(
                 '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                 datefmt="%d/%m/%Y %H:%M:%S"
             )
@@ -60,7 +60,7 @@ class LoggerManager:
             logger.addHandler(file_handler)
 
             # Console Handler
-            console_handler = logging.StreamHandler()
+            console_handler = l.StreamHandler()
             console_handler.setLevel(log_level)
             console_formatter = ColoredFormatter(
                 '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -72,19 +72,19 @@ class LoggerManager:
         self._loggers[name] = logger
         return logger
 
-    def get_logger(self) -> logging.Logger:
+    def get_logger(self) -> l.Logger:
         return self.logger
 
 
-class ColoredFormatter(logging.Formatter):
+class ColoredFormatter(l.Formatter):
     def format(self, record) -> str:
-        if record.levelno == logging.INFO:
+        if record.levelno == l.INFO:
             record.msg = Fore.LIGHTWHITE_EX + str(record.msg) + Style.RESET_ALL
-        elif record.levelno == logging.WARNING:
+        elif record.levelno == l.WARNING:
             record.msg = Fore.YELLOW + str(record.msg) + Style.RESET_ALL
-        elif record.levelno == logging.ERROR:
+        elif record.levelno == l.ERROR:
             record.msg = Fore.RED + str(record.msg) + Style.RESET_ALL
-        elif record.levelno == logging.CRITICAL:
+        elif record.levelno == l.CRITICAL:
             record.msg = Fore.YELLOW + Back.RED + str(record.msg) + Style.RESET_ALL
 
         return super().format(record)
